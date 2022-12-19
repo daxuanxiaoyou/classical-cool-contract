55c290b6c34685be4a1cca03456266f3f64a69b1
daxuanxiaoyou <yangsai_2006@163.com>
2022-12-19 19:34:02 +0800

    add reviews

--- contracts/ClassicalBookNFT.sol
+++ contracts/ClassicalBookNFT.sol
@@ -21,6 +21,22 @@ contract ClassicalBookNFT is ERC721, PullPayment, Ownable {
     // name and symbol
     constructor() ERC721("Classical Book NFT Collectioin", "ClassicalBook") {}
 
+    //review by vincent:
+    //tokenId可以从0开始，在调用mintTo时通过currentTokenId.current()得到当前可用的id作为本次铸造的tokenId；然后再currentTokenId.increment()进行递增
+    //从方法实现上看，铸造NFT没有收费，可以把payable去掉
+    /*
+        function mintTo(address recipient, string memory bookId)
+            public
+            returns (uint256)
+        {
+            uint256 tokenId = currentTokenId.current();
+            require(tokenId < maxSupply, "Max supply reached");
+            emit mintEvent(tokenId, bookId, recipient);
+            _safeMint(recipient, tokenId);
+            currentTokenId.increment();
+            return tokenId;
+        }
+    */
     function mintTo(address recipient, string memory bookId)
         public
         payable
@@ -47,6 +63,9 @@ contract ClassicalBookNFT is ERC721, PullPayment, Ownable {
     }
 
     /// Sets max supply, one book one nft
+    //review by vincent:需要确保 _maxSupply不能小于最新的tokenId
+    //uint256 tokenId = currentTokenId.current();
+    //require(_maxSupply > tokenId, "Must greate than current token id");
     function setMaxSupply(uint256 _maxSupply) public onlyOwner {
         maxSupply = _maxSupply;
     }
@@ -54,6 +73,8 @@ contract ClassicalBookNFT is ERC721, PullPayment, Ownable {
     /**
      * @dev See {IERC721Enumerable-totalSupply}.
      */
+    //review by vincent:
+    //因为tokenId是从0开始，所以需要return tokenId + 1;
     function totalSupply() public view returns (uint256) {
         uint256 tokenId = currentTokenId.current();
         return tokenId;
@@ -64,6 +85,7 @@ contract ClassicalBookNFT is ERC721, PullPayment, Ownable {
     }
 
     /// @dev Overridden in order to make it an onlyOwner function
+    //review by vincent:没有deposit接口，withdraw没有意义？这个接口的设计是什么意思？？
     function withdrawPayments(address payable payee)
         public
         virtual
